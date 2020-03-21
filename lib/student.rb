ac03872ce7ceb523dd6a67a092496977f630a7f7
Daniel Sun <danielsun12345@gmail.com>
2020-03-21 03:15:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -72,6 +72,17 @@ class Student
     end 
   end 
   
+  def self.students_below_12th_grade
+    sql = <<-SQL 
+      SELECT * 
+      FROM students
+      WHERE grade = 9,10,11
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+  end 
   
   
   
