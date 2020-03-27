1c2bb7799ba976ec31819c6e019605f22977a35f
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 15:28:27 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -93,4 +93,38 @@ class Student
       self.new_from_db(row)
     end
   end 
+  
+  def self.first_X_students_in_grade_10(num)
+     sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE grade = 10 
+      LIMIT ?
+      SQL
+      DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+  
+  def self.first_student_in_grade_10
+     sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE grade = 10 
+      SQL
+      DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE grade = ? 
+      SQL
+      DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row) 
+    end 
+  end
 end
