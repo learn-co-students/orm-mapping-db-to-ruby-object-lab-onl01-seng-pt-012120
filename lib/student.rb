2b76c4e6cc992fd8dd90b9dbdc0282ec135d83b1
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-17 17:40:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -41,8 +41,7 @@ class Student
   end
 
   def self.all_students_in_grade_X(x)
-     self.all.select{|student| student.grade == x}.to_a
-     binding.pry
+     self.all.select{|student| student.grade == x.to_s}.to_a
   end
 
   def save
